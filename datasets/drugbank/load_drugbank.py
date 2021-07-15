7f72653ecb779d4688127bf45152f9d8bfbd2f9a
Vincent Emonet <vincent.emonet@gmail.com>
2021-07-15 09:46:41 +0000

    Add pubmed ID processing

--- datasets/drugbank/load_drugbank.py
+++ datasets/drugbank/load_drugbank.py
@@ -20,12 +20,14 @@ def get_path(filename=''):
 
 # Setup timer and logging
 start_time = datetime.datetime.now()
+print("path")
+print(get_path())
 os.makedirs(get_path(), exist_ok=True)
 log.basicConfig(filename=get_path('run.log'), filemode='w', level=log.DEBUG,
     datefmt='%Y-%m-%d %H:%M:%S', format='%(asctime)s %(levelname)-8s %(message)s')
 log.getLogger().addHandler(log.StreamHandler(sys.stdout))
 
-# Set those variables as GitHub secrets for drugbank download
+# Set those environment variables as GitHub secrets for drugbank download
 drugbank_username = os.getenv('DRUGBANK_USERNAME', 'vincent.emonet@maastrichtuniversity.nl')
 drugbank_password = os.getenv('DRUGBANK_PASSWORD', 'changepassword')
 drugbank_version = os.getenv('DRUGBANK_VERSION', '5-1-8')
@@ -38,7 +40,10 @@ log.info('💽 Loading the drugbank.zip file')
 # zip_path = os.path.join('data', 'drugbank.zip')
 archive = zipfile.ZipFile(get_path('drugbank.zip'), 'r')
 
-with archive.open('full database.xml', 'r') as xml_file:
+
+# Script based on https://github.com/dhimmel/drugbank/blob/gh-pages/parse.ipynb
+# with archive.open('full database.xml', 'r') as xml_file:
+with open(get_path('sample_drugbank.xml')) as xml_file:
     log.info('Zip loaded, parsing the XML...')
     tree = ET.parse(xml_file)
     
@@ -64,6 +69,9 @@ for i, drug in enumerate(root):
         drug.findall("{ns}categories/{ns}category".format(ns = ns))]
     row['inchi'] = drug.findtext(inchi_template.format(ns = ns))
     row['inchikey'] = drug.findtext(inchikey_template.format(ns = ns))
+    
+    row['pubmed_refs'] = [x.findtext(ns + 'pubmed-id') for x in
+        drug.findall("{ns}general-references/{ns}articles/{ns}article".format(ns = ns))]
     
     # Add drug aliases
     aliases = {
@@ -91,12 +99,20 @@ def collapse_list_values(row):
 
 rows = list(map(collapse_list_values, rows))
 
-columns = ['drugbank_id', 'name', 'type', 'groups', 'atc_codes', 'categories', 'inchikey', 'inchi', 'description']
+columns = ['drugbank_id', 'name', 'type', 'groups', 'atc_codes', 'categories', 'inchikey', 'inchi', 'description', 'pubmed_refs']
 drugbank_df = pandas.DataFrame.from_dict(rows)[columns]
 
 print(drugbank_df.head())
 
 drugbank_df.to_csv(get_path('drugbank_processed.csv'))
+
+# Only drug approved with InChi
+# drugbank_slim_df = drugbank_df[
+#     drugbank_df.groups.map(lambda x: 'approved' in x) &
+#     drugbank_df.inchi.map(lambda x: x is not None) &
+#     drugbank_df.type.map(lambda x: x == 'small molecule')
+# ]
+# print(drugbank_slim_df.head())
 
 run_time = datetime.datetime.now() - start_time
 log.info('Ran for ' + str(run_time))
