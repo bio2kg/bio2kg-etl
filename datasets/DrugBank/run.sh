4d1c6d467cf57e0a64734ac0644fcf7f684b60aa
Vincent Emonet <vincent.emonet@gmail.com>
2021-08-06 17:45:22 +0200

    improve drugbank workflow

--- datasets/DrugBank/run.sh
+++ datasets/DrugBank/run.sh
@@ -16,17 +16,7 @@ echo "Convert YARRRML to RML mappings"
 yarrrml-parser -i drugbank-mapping.yarrr.yml -o data/mapping.rml.ttl
 
 echo "Running RML mapper, output to data/ folder"
-java $1 -jar /opt/rmlmapper.jar -m data/mapping.rml.ttl -o data/bio2kg-drugbank.ttl -s turtle -f ../functions_ids.ttl 
+java $1 -jar /opt/rmlmapper.jar -m data/mapping.rml.ttl -o data/bio2kg-drugbank.ttl -s turtle
 
+# java $1 -jar /opt/rmlmapper.jar -m data/mapping.rml.ttl -o data/bio2kg-drugbank.ttl -s turtle -f ../functions_ids.ttl 
 
-
-##Example to run quick python scripts:
-# pip install pandas
-# python3 <<HEREDOC
-# import pandas as pd
-# csv_table=pd.read_table('hgnc_complete_set.tsv',sep='\t')
-# csv_table.applymap
-# s = pd.Series(csv_table)
-# s.str.strip()
-# csv_table.to_csv('hgnc_complete_set.csv',index=False)
-# HEREDOC
--- prepare_local.sh
+++ prepare_local.sh
@@ -1,3 +1,19 @@
 
+echo "Downloading rmlmapper.jar in /opt"
+curl -s https://api.github.com/repos/RMLio/rmlmapper-java/releases/latest \
+    | grep browser_download_url | grep .jar | cut -d '"' -f 4 \
+    | wget -O /opt/rmlmapper.jar -qi -
+
+echo "Installing YARRRML parser"
+yarn global add @rmlio/yarrrml-parser
+
+echo "Install dependencies for python scripts"
+conda install --file requirements.txt
+# or use pip install -r requirements.txt
+
+# Additional packages to install
+# conda install --quiet -y wget unzip
+
+echo "Retrieving IDS RML functions files"
 cp ../ids-rml-functions/functions_ids.ttl datasets/
 cp ../ids-rml-functions/target/IdsRmlFunctions-v0.0.1-jar-with-dependencies.jar datasets/IdsRmlFunctions.jar
