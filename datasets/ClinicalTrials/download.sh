7a4369a116923591478a144019b7b6c6b82cb9b9
Vincent Emonet <vincent.emonet@gmail.com>
2021-08-23 17:11:08 +0200

    fix ClinicalTrials ddl

--- datasets/ClinicalTrials/download.sh
+++ datasets/ClinicalTrials/download.sh
@@ -5,6 +5,6 @@ if [ ! -f "data/AllPublicXML.zip" ]; then
     echo "data/AllPublicXML.zip does not exist, downloading."
     mkdir -p data/all-public-xml
     cd data
-    wget -N  https://unmtid-shinyapps.net/download/drug.target.interaction.tsv.gz
+    wget -N https://clinicaltrials.gov/AllPublicXML.zip
     unzip AllPublicXML.zip -d all-public-xml
 fi
